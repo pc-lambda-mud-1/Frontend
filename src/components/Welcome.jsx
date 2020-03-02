055b333787aae1adb34a241b7085703365ae55f8
Dom Eccleston <domeccleston@gmail.com>
2020-03-02 19:16:11 +0000

    basic logout endpoint

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const Welcome = () => {
-    return (
-        <>
-        <div>Welcome!</div>
-        </>
-    )
-}
+  const history = useHistory();
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push("/");
+  };
+  return (
+    <>
+      <div>Welcome!</div>
+      <button onClick={handleLogout}>Logout</button>
+    </>
+  );
+};
 
 export default Welcome;

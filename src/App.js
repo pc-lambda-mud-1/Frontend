79b5a681bdf0c3b074bf3e70ec4e51cfc22c6ad2
Dom Eccleston <domeccleston@gmail.com>
2020-03-03 13:08:50 +0000

    push latest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Register from './components/Register';
 import Login from './components/Login';
+import Welcome from './components/Welcome'
+import { Route } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Login />
+      <Route exact path='/' component={() => <><Register /><Login /></>} />
+      <PrivateRoute exact path='/welcome' component={Welcome} />
     </div>
   );
 }

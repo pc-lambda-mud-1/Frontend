00d8010de9b4fb081a48ff893f0dc64ebca77e06
Dom Eccleston <domeccleston@gmail.com>
2020-03-03 15:18:35 +0000

    wip

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,40 +1,69 @@
-import React from "react";
-import {
-  ChatkitProvider,
-  TokenProvider,
-  withChatkit
-} from "@pusher/chatkit-client-react";
+// src/App.js
+import React, { Component } from "react";
+import ChatMessage from "./ChatMessage";
+import Signup from "./Signup";
+import { default as Chatkit } from "@pusher/chatkit-server";
 
-const tokenProvider = new TokenProvider({
-  url:
-    "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/6975310b-1b8c-46eb-8e37-266c835807a7/token"
+const chatkit = new Chatkit({
+  instanceLocator: "v1:us1:6975310b-1b8c-46eb-8e37-266c835807a7",
+  key:
+    "9ee83a08-1ddf-4a99-996f-bc80248ca393:Ip4DZWroeixciUZ/fnCW+P7i/bZ2GnHKvFfHBimu8ww="
 });
 
-const userId = "sam";
-const instanceLocator = "v1:us1:6975310b-1b8c-46eb-8e37-266c835807a7";
+class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentView: "ChatMessage",
+      currentId: "",
+      currentUsername: ""
+    };
+    this.changeView = this.changeView.bind(this);
+    this.createUser = this.createUser.bind(this);
+  }
 
-const WelcomeMessage = withChatkit(props => {
-    return (
-        <div>
-            {props.chatkit.isLoading
-            ? 'Connecting to chatkit...'
-            : `Hello ${props.chatkit.currentUser.name}!`}
-        </div>
-    )
-})
+  createUser(username) {
+    chatkit
+      .createUser({
+        id: username,
+        name: username
+      })
+      .then(currentUser => {
+        this.setState({
+          currentUsername: username,
+          currentId: username,
+          currentView: "chatApp"
+        });
+      })
+      .catch(err => {
+        if (err.status === 400) {
+          this.setState({
+            currentUsername: username,
+            currentId: username,
+            currentView: "chatApp"
+          });
+        } else {
+          console.log(err.status);
+        }
+      });
+  }
 
-const Chat = () => {
-  return (
-    <div>
-      <ChatkitProvider
-        instanceLocator={instanceLocator}
-        tokenProvider={tokenProvider}
-        userId={userId}
-      >
-          <WelcomeMessage />
-      </ChatkitProvider>
-    </div>
-  );
-};
+  changeView(view) {
+    this.setState({
+      currentView: view
+    });
+  }
 
+  render() {
+    let view = "";
+    if (this.state.currentView === "ChatMessage") {
+      view = <ChatMessage changeView={this.changeView} />;
+    } else if (this.state.currentView === "signup") {
+      view = <Signup onSubmit={this.createUser}/>;
+    } else if (this.state.currentView === "chatApp") {
+        view = <h1>The chatapp will go here</h1>
+    }
+    return <div className="Chat">{view}</div>;
+  }
+}
 export default Chat;
